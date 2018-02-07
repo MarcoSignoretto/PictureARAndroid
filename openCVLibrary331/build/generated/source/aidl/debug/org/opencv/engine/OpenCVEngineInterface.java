686852d9fd0a4ca61ffa5e7fb862a08fe83d16a1
Marco Signoretto <marco.signoretto.dev@gmail.com>
2018-02-07 17:47:24 +0100

    Removed files from versioning

--- openCVLibrary331/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary331/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/marco/Desktop/OpenCV_test/openCVLibrary331/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/marco/Documents/GitProjects/Android/PERSONAL/PictureARAndroid/openCVLibrary331/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
