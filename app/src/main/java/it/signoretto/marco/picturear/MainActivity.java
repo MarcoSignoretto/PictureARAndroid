6e0ba95803dd53f48c15374f11187142c250266e
Marco Signoretto <marco.signoretto.dev@gmail.com>
2024-01-25 08:28:29 +0400

    Bumping dependencies and migrated to androidx

--- app/src/main/java/it/signoretto/marco/picturear/MainActivity.java
+++ app/src/main/java/it/signoretto/marco/picturear/MainActivity.java
@@ -9,7 +9,7 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
